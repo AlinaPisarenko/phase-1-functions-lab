8a46efdee9ebff83e26215b5671ddfe8f6030fea
Alina <pisarenko.alina@gmail.com>
2022-04-05 10:02:19 -0400

    commit tree 2376eee58265400c55677e260cc1c682e3fe5a1a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const location = 42;
+  const result = Math.abs(someValue - location);
+  return result;
+}
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((start - destination) * 264);
+}
+function calculatesFarePrice(start, destination) {
+  let someResult = distanceTravelledInFeet(start, destination);
+  if (someResult <= 400) return 0;
+  else if (someResult > 400 && someResult <= 2000)
+    return (someResult - 400) * 0.02;
+  else if (someResult > 2000 && someResult <= 2500) return 25;
+  else if (someResult > 2500) return `cannot travel that far`;
+}
